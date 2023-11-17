75f70846583d224b24f23a670c8c46847dfbbf06
Aime-Tresor Ndayongeje <aime-tresor.ndayongeje@etu.univ-grenoble-alpes.fr>
2023-11-17 12:34:30 +0100

    iobuf_write

--- stdes.c
+++ stdes.c
@@ -25,6 +25,14 @@ IOBUF_FILE *iobuf_open(const char *nom, char mode) {
     return f;
 }
 
+/* Ecriture dans le buffer système */
+void ecrire_buffer_systeme(IOBUF_FILE *f, const void *p, int size){
+    for (int i = f->buf_size; i < f->buf_size + size; i++){
+        ((char*)f->buf)[i] = ((char*)p)[i - f->buf_size];
+    }
+    f->buf_size = f->buf_size + size;
+}
+
 int iobuf_close(IOBUF_FILE *f) {
     if (f == NULL) return -1;
     if (iobuf_flush(f) < 0) return -1;
@@ -78,7 +86,40 @@ int iobuf_read(void *p, unsigned int taille, unsigned int nbelem, IOBUF_FILE *f)
 
 
 int iobuf_write(const void *p, unsigned int taille, unsigned int nbelem, IOBUF_FILE *f) {
-    return 0;
+    if (f->mode != 'W' || p == NULL){
+        return -1;
+    }
+
+    int size = taille * nbelem;
+
+    if ((f->buf_size + size) <= BUFFER_SIZE){
+        ecrire_buffer_systeme(f, p, size);
+        return nbelem;
+    }
+
+    else{
+        int size_written = 0;   // Nombre d'octets déjà écrits
+        int n_elem_restant = nbelem;    // Nombre d'éléments à lire
+        int n_elem_buf = 0;  // Nombre d'éléments à écrire dans le buffer
+
+        // Tant que le nombre d'éléments non encore écrit ne rentre pas  dans le buffer,
+        // On remplit le buffer avec ce qu'on peut et on fait write avant de le vider
+        while(taille*n_elem_restant > (BUFFER_SIZE - f->buf_size)){
+            n_elem_buf = (BUFFER_SIZE - f->buf_size)/taille;
+            ecrire_buffer_systeme(f, p+size_written, taille*n_elem_buf);
+            iobuf_flush(f);
+            size_written += taille*n_elem_buf;
+            n_elem_restant -= n_elem_buf;
+        }
+
+        // Si le nombre d'élements restant rentre dans le buffer, on les met dans le buffer
+        if(n_elem_restant != 0){
+            ecrire_buffer_systeme(f, p+size_written, taille*n_elem_restant);
+            size_written += taille*n_elem_buf;
+        }
+
+        return (nbelem - n_elem_restant);
+    }
 }
 
 int iobuf_flush(IOBUF_FILE *f) {
